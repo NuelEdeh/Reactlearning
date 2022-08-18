db66709585c4dcd0251ec94a76245947697500e2
Nuel Edeh <nuel@getneat.io>
2022-08-18 18:13:14 -0400

    first-react-project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Tweet from "./Tweet";
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [users, setUsers] = useState([
+    { name: "Nuel", message: "Hello there" },
+    { name: "John", message: "I am John Snow" },
+    { name: "Alex", message: "I am pretty cool" },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {users.map((user) => (
+        <Tweet name={user.name} message={user.message} />
+      ))}
     </div>
   );
 }
